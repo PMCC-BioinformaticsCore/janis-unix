ac525aa757d8266bcf6b0c065a3fb35f9d23b097
Thomas Conway <drtomc@gmail.com>
2020-06-09 16:35:22 +1000

    add a binding for MD5 sums.

--- janis_unix/tools/__init__.py
+++ janis_unix/tools/__init__.py
@@ -2,6 +2,7 @@ from .awk import Awk
 from .cat import Cat
 from .compile import Compile
 from .echo import Echo
+from .md5sum import MD5Sum
 from .sleep import Sleep
 from .tar import Tar
 from .untar import Untar
