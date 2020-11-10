0a928b7556fd0114fc72d2b57b9255b307278fcd
Michael Franklin <michael.franklin@petermac.org>
2020-11-10 13:01:43 +1100

    Pin version of unixtool to specific sha@256:1d7b639

--- janis_unix/tools/unixtool.py
+++ janis_unix/tools/unixtool.py
@@ -8,7 +8,7 @@ class UnixTool(CommandTool, ABC):
         return "unix"
 
     def container(self):
-        return "ubuntu:latest"
+        return "ubuntu@sha256:1d7b639619bdca2d008eca2d5293e3c43ff84cbee597ff76de3b7a7de3e84956"
 
     def version(self):
         return "v1.0.0"
