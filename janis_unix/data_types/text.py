a46c211141d78e272b674c6db948eae716cb563e
xinzhel97 <l383562940@gmail.com>
2021-04-12 16:44:40 +1000

    Add test for TextFile data type and unit test for UncompressArchive

--- janis_unix/data_types/text.py
+++ janis_unix/data_types/text.py
@@ -1,4 +1,8 @@
+import operator
+from typing import Optional
+
 from janis_core import File
+from janis_core.tool.test_classes import TTestExpectedOutput, TTestPreprocessor
 
 
 class TextFile(File):
@@ -11,3 +15,43 @@ class TextFile(File):
 
     def doc(self):
         return "A textfile, ending with .txt"
+
+    @classmethod
+    def basic_test(
+        cls,
+        tag: str,
+        min_size: int,
+        content: str,
+        line_count: int,
+        md5: Optional[str] = None,
+    ):
+        outcome = [
+            TTestExpectedOutput(
+                tag=tag,
+                preprocessor=TTestPreprocessor.FileSize,
+                operator=operator.ge,
+                expected_value=min_size,
+            ),
+            TTestExpectedOutput(
+                tag=tag,
+                preprocessor=TTestPreprocessor.FileContent,
+                operator=operator.contains,
+                expected_value=content,
+            ),
+            TTestExpectedOutput(
+                tag=tag,
+                preprocessor=TTestPreprocessor.LineCount,
+                operator=operator.eq,
+                expected_value=line_count,
+            ),
+        ]
+        if md5 is not None:
+            outcome += [
+                TTestExpectedOutput(
+                    tag=tag,
+                    preprocessor=TTestPreprocessor.FileMd5,
+                    operator=operator.eq,
+                    expected_value=md5,
+                ),
+            ]
+        return outcome
