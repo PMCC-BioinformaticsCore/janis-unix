481d02ab16ff44fb623e18fdc965ee2ad4212686
xinzhel97 <l383562940@gmail.com>
2021-04-13 12:41:06 +1000

    Make some parameters optional for TextFile test

--- janis_unix/data_types/text.py
+++ janis_unix/data_types/text.py
@@ -1,5 +1,5 @@
 import operator
-from typing import Optional
+from typing import Optional, List
 
 from janis_core import File
 from janis_core.tool.test_classes import TTestExpectedOutput, TTestPreprocessor
@@ -21,10 +21,10 @@ class TextFile(File):
         cls,
         tag: str,
         min_size: int,
-        content: str,
-        line_count: int,
+        min_required_content: Optional[str] = None,
+        line_count: Optional[int] = None,
         md5: Optional[str] = None,
-    ):
+    ) -> List[TTestExpectedOutput]:
         outcome = [
             TTestExpectedOutput(
                 tag=tag,
@@ -32,19 +32,28 @@ class TextFile(File):
                 operator=operator.ge,
                 expected_value=min_size,
             ),
-            TTestExpectedOutput(
-                tag=tag,
-                preprocessor=TTestPreprocessor.FileContent,
-                operator=operator.contains,
-                expected_value=content,
-            ),
-            TTestExpectedOutput(
-                tag=tag,
-                preprocessor=TTestPreprocessor.LineCount,
-                operator=operator.eq,
-                expected_value=line_count,
-            ),
         ]
+
+        if min_required_content is not None:
+            outcome += [
+                TTestExpectedOutput(
+                    tag=tag,
+                    preprocessor=TTestPreprocessor.FileContent,
+                    operator=operator.contains,
+                    expected_value=min_required_content,
+                ),
+            ]
+
+        if line_count is not None:
+            outcome += [
+                TTestExpectedOutput(
+                    tag=tag,
+                    preprocessor=TTestPreprocessor.LineCount,
+                    operator=operator.eq,
+                    expected_value=line_count,
+                ),
+            ]
+
         if md5 is not None:
             outcome += [
                 TTestExpectedOutput(
@@ -54,4 +63,5 @@ class TextFile(File):
                     expected_value=md5,
                 ),
             ]
+
         return outcome
