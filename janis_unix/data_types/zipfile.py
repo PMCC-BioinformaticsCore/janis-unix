224ca0addee8fe85c7fb01246f74e14723972d4d
xinzhel97 <l383562940@gmail.com>
2021-04-21 14:28:01 +1000

    Add unit test for zipfile data type and a draft array wrapper

--- janis_unix/data_types/zipfile.py
+++ janis_unix/data_types/zipfile.py
@@ -1,4 +1,8 @@
+import operator
+from typing import Optional, List, Callable, Dict
+
 from janis_core import File
+from janis_core.tool.test_classes import TTestExpectedOutput, TTestPreprocessor
 
 
 class ZipFile(File):
@@ -11,3 +15,51 @@ class ZipFile(File):
 
     def doc(self):
         return "A zip archive, ending with .zip"
+
+    @classmethod
+    def basic_test(
+        cls,
+        tag: str,
+        min_size: int,
+        md5: Optional[str] = None,
+        array_index: Optional[int] = None,
+    ) -> List[TTestExpectedOutput]:
+        outcome = [
+            TTestExpectedOutput(
+                tag=tag,
+                preprocessor=TTestPreprocessor.FileSize,
+                operator=operator.ge,
+                expected_value=min_size,
+                array_index=array_index,
+            ),
+        ]
+        if md5 is not None:
+            outcome += [
+                TTestExpectedOutput(
+                    tag=tag,
+                    preprocessor=TTestPreprocessor.FileMd5,
+                    operator=operator.eq,
+                    expected_value=md5,
+                    array_index=array_index,
+                ),
+            ]
+        return outcome
+
+    @classmethod
+    def array_wrapper(
+        cls,
+        tag: str,
+        test: Callable,
+        list_size: int,
+        expected_values: Optional[Dict] = {},
+    ) -> List[TTestExpectedOutput]:
+        outcome = []
+        for i in range(list_size):
+            expected_values_for_one_file = {}
+            for j in expected_values.keys():
+                expected_values_for_one_file[j] = expected_values[j][i]
+            try:
+                outcome += test(tag=tag, array_index=i, **expected_values_for_one_file)
+            except TypeError:
+                print("Wrong arguments passed to " + test.__name__)
+        return outcome
